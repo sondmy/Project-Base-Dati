e4cc91fc8e25b24c344d9ada78dcdcb12885c23b
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-06-23 16:16:37 +0200

    clear

--- src/main/java/it/unibo/zoo/model/jdbc/StatementBinder.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.zoo.model.jdbc;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface StatementBinder {
-    void bind(PreparedStatement statement) throws SQLException;
-}
